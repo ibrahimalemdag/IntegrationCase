9b70b14cf57ddd618b17529b7329d57db9f775a7
ibrahimalemdag <alemdagibrahim@gmail.com>
2023-12-21 23:46:51 +0300

    Add files via upload

--- Integration/Program.cs
+++ Integration/Program.cs
@@ -1,4 +1,14 @@
-﻿using Integration.Service;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using EventBus.EventBusRabbitMQ;
+using EventBus.Library;
+using EventBus.Library.Abstractions;
+using Integration.Service;
+using IntegrationEvents.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 
 namespace Integration;
 
@@ -6,19 +16,24 @@ public abstract class Program
 {
     public static void Main(string[] args)
     {
-        var service = new ItemIntegrationService();
-        
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("b"));
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("c"));
+        var host = CreateHostBuilder(args).Build();
+        var eventBus = host.Services.GetRequiredService<IEventBus>();
+        eventBus.Subscribe<ResourceTakenIntegrationEvent,
+            IIntegrationEventHandler<ResourceTakenIntegrationEvent>>("testQueue");
+
+        var service = host.Services.GetRequiredService<IIntegrationService>();
+
+        ThreadPool.QueueUserWorkItem(_ => service.TrySaveItem("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.TrySaveItem("b"));
+        ThreadPool.QueueUserWorkItem(_ => service.TrySaveItem("c"));
 
         Thread.Sleep(500);
 
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("b"));
-        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("c"));
+        ThreadPool.QueueUserWorkItem(_ => service.TrySaveItem("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.TrySaveItem("b"));
+        ThreadPool.QueueUserWorkItem(_ => service.TrySaveItem("c"));
 
-        Thread.Sleep(5000);
+        Thread.Sleep(6000);
 
         Console.WriteLine("Everything recorded:");
 
@@ -26,4 +41,42 @@ public abstract class Program
 
         Console.ReadLine();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+            {
+                var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+
+                var factory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    DispatchConsumersAsync = false
+                };
+
+                return new DefaultRabbitMQPersistentConnection(factory, logger);
+            });
+
+            services.AddSingleton<IEventBus, EventBusRabbitMQ>(sp =>
+            {
+                var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
+                var iLifetimeScope = sp.GetRequiredService<ILifetimeScope>();
+                var logger = sp.GetRequiredService<ILogger<EventBusRabbitMQ>>();
+                var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+
+                return new EventBusRabbitMQ(rabbitMQPersistentConnection, logger, iLifetimeScope, eventBusSubcriptionsManager);
+            });
+
+            services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
+
+            services.AddSingleton<IIntegrationEventHandler<ResourceTakenIntegrationEvent>,
+                                                                ResourceTakenIntegrationEventHandler>();
+
+            services.AddSingleton<IIntegrationService, ItemIntegrationService>();
+
+
+        });
 }

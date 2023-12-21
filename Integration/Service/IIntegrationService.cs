513fa12323f6c86c91f18cace94317ac3ff8bc90
ibrahimalemdag <alemdagibrahim@gmail.com>
2023-12-21 23:49:32 +0300

    Add files via upload

---
+++ Integration/Service/IIntegrationService.cs
@@ -0,0 +1,12 @@
+﻿
+using Integration.Common;
+
+namespace Integration.Service
+{
+    internal interface IIntegrationService
+    {
+        public Result TrySaveItem(string itemContent);
+        public void SaveItem(string itemContent);
+        public List<Item> GetAllItems();
+    }
+}
--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -1,10 +1,21 @@
 ﻿using Integration.Common;
 using Integration.Backend;
+using EventBus.Library.Abstractions;
+using IntegrationEvents.Events;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
 
 namespace Integration.Service;
 
-public sealed class ItemIntegrationService
+public sealed class ItemIntegrationService : IIntegrationService
 {
+    IEventBus EventBus;
+
+    public ItemIntegrationService(IEventBus eventBus)
+    {
+        EventBus = eventBus;
+    }
+
     //This is a dependency that is normally fulfilled externally.
     private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
 
@@ -12,21 +23,33 @@ public sealed class ItemIntegrationService
     // More than one item with the same content should not be saved. However,
     // calling this with different contents at the same time is OK, and should
     // be allowed for performance reasons.
-    public Result SaveItem(string itemContent)
+    public Result TrySaveItem(string itemContent)
     {
+        Monitor.Enter(itemContent);
         // Check the backend to see if the content is already saved.
         if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
         {
             return new Result(false, $"Duplicate item received with content {itemContent}.");
         }
 
-        var item = ItemIntegrationBackend.SaveItem(itemContent);
+        EventBus.Publish(new SaveRequestAcceptedIntegrationEvent(itemContent));
 
-        return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+        Monitor.Exit(itemContent);
+        return new Result(true, $"Item with content {itemContent} saved");
     }
 
     public List<Item> GetAllItems()
     {
         return ItemIntegrationBackend.GetAllItems();
+    }
+
+    public void SaveItem(string itemContent)
+    {
+        if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+        {
+            return;
+        }
+
+        ItemIntegrationBackend.SaveItem(itemContent);
     }
 }
